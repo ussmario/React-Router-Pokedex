20f0e94b1c5e2ee494a3a03d9861e7f52407787e
floydthormodson <floydthormodson@outlook.com>
2021-10-15 14:45:29 -0400

    Implement MUI, parse out details.

--- src/Components/PokemonDetails.js
+++ src/Components/PokemonDetails.js
@@ -1,10 +1,17 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Box from '@mui/material/Box';
 
 
-export default function PokemonDetails({favoriteFuncs}) {
+
+export default function PokemonDetails({ favoriteFuncs }) {
     const { pokemonName } = useParams();
-    const [pokemon, setPokemon] = useState({});
+    const [pokemon, setPokemon] = useState();
     const [checked, setChecked] = useState(favoriteFuncs.isFavorite(pokemonName));
 
     useEffect(() => {
@@ -17,30 +24,56 @@ export default function PokemonDetails({favoriteFuncs}) {
         setChecked(favoriteFuncs.isFavorite(pokemonName));
     }, [pokemonName]);
 
-   
+
     const handleChange = () => {
-        if(checked){
+        if (checked) {
             favoriteFuncs.removeFavorite(pokemonName)
-        }else{
+        } else {
             favoriteFuncs.addFavorite(pokemonName)
         }
         setChecked(!checked);
     };
 
+    function capitalize(str) {
+        return str[0].toUpperCase() + str.substring(1)
+    }
+
+    if(!pokemon){
+        return(<h1>Loading</h1>)
+    }
     return (
-        <>
-            <label>
-                <input type="checkbox" checked={checked} onChange={handleChange}/>
-                Favorites
-            </label>
-            <hr />
-            <div>
-                {JSON.stringify(pokemon)}
-            </div>
-        </>
+        <Box sx={{ mx: "auto", width: "100%" }}>
+            <Card sx={{ margin: 5 }}>
+                <CardMedia
+                    component="img"
+                    // height="140"
+                    sx={{ width: "25vw", display: "inline-flex", mx: "auto" }}
+                    image={pokemon?.sprites?.other["official-artwork"].front_default}
+                    alt="pokemon sprite"
+                />
+                <CardContent sx={{ mx: "auto", display:"table" }}>
+                    <Typography gutterBottom variant="h2" component="div">
+                        {capitalize(pokemon.name)}
+                    </Typography>
 
+                    <Typography gutterBottom variant="h5" component="div">
+                        <label>
+                            <input type="checkbox" checked={checked} onChange={handleChange} />
+                            Favorite
+                        </label>
+                    </Typography>
+                    
+                    <Typography variant="body2" color="text.secondary">
+                        <h3>Type(s): {pokemon.types.map((type, index) => <div key={index+ 1000}>{capitalize(type.type.name)}</div>)}</h3>
+                        <h3>PokeDex Entry #: {pokemon.id}</h3>
+                        <h3>Ability: {capitalize(pokemon.abilities[0].ability.name)}</h3>
+                        
 
-    )
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Box>
+    );
 
 
 
